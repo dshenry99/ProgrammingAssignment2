249029950e245e690eb0f14fbedf6f7cdb007555
Dave Henry <ds_henry@yahoo.com>
2015-04-25 18:10:36 -0700

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+# makeCacheMatrix creates a list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        } 
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
 
+        getmatrix <- function() m
+        list(set = set, get = get,
+        setmatrix = setmatrix,
+        getmatrix = getmatrix)
 }
 
 
@@ -12,4 +24,45 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
+
+
+## Note: the following lines of code were used to test the makeCacheMatrix() and cacheSolve() functions
+
+# m <- matrix(c(-1, -2, 1, 1), 2,2)
+# x <- makeCacheMatrix(m)
+# m
+# inv <- cacheSolve(x)
+# inv
+# inv <- cacheSolve(x)
+# inv
+# 
+# ## produces the following results
+# 
+# > m <- matrix(c(-1, -2, 1, 1), 2,2)
+# > x <- makeCacheMatrix(m)
+# > m
+# [,1] [,2]
+# [1,]   -1    1
+# [2,]   -2    1
+# > inv <- cacheSolve(x)
+# > inv
+# [,1] [,2]
+# [1,]    1   -1
+# [2,]    2   -1
+# > inv <- cacheSolve(x)
+# getting cached data
+# > inv
+# [,1] [,2]
+# [1,]    1   -1
+# [2,]    2   -1
+# > 
